db524e3e22b4b94f80cc36cadadfe7dfe6f287ab
Shani <shani@Shanis-iMac.local>
2016-11-13 11:21:31 -0800

    Updated some of the info and reformatted

--- TDD_Setup.swift
+++ TDD_Setup.swift
@@ -1,4 +1,11 @@
+// TDD In Swift Playgrounds - forked from sshrpe and modified by Shani https://github.com/shanirivers/TDDSwiftPlayground
+// Practice with Katas  http://agilekatas.co.uk/katas
+
+// First: make sure you import XCTest:
+
 import XCTest
+
+
 
 // TYPE IMPLEMENTATION CODE HERE:
 
@@ -6,46 +13,56 @@ import XCTest
 
 
 
-// TYPE TESTS HERE:
-class MyExampleTests : XCTestCase {
-    override func setUpExample() {
+
+
+
+
+// TEST GO HERE:
+
+class MyTests : XCTestCase {
+    func testShouldFail() {
+        XCTFail("You must fail to succeed!")
     }
-    
-    override func tearDownExample() {
-    }
-	
-    func testingExample_ShouldBeTrue() {
-	var myNumber = 3
-	XCTAssertTrue(myNumber, 3)
+    func testShouldPass() {
+        XCTAssertEqual(2 + 2, 4)
     }
 }
 
+class MyOtherTest: XCTestCase {
+    
+}
 
 // **************** BOILERPLATE CODE FOR RUNNING TESTS TO OUTPUT IN COMMAND LINE **************** //
+
 class PlaygroundTestObserver : NSObject, XCTestObservation {
     @objc func testCase(_ testCase: XCTestCase, didFailWithDescription description: String, inFile filePath: String?, atLine lineNumber: UInt) {
-        print("\nTest failed on line \(lineNumber): \(testCase.name), \(description)\n")
+        print("Test failed on line \(lineNumber): \(testCase.name), \(description)")
     }
 }
 
-let playgroundObserver = PlaygroundTestObserver()
-let testObservationCenter = XCTestObservationCenter.shared()
-testObservationCenter.addTestObserver(playgroundObserver)
+let observer = PlaygroundTestObserver()
+let center = XCTestObservationCenter.shared()
+center.addTestObserver(observer)
 
 
 struct TestRunner {
+    
     func runTests(testClass:AnyClass) {
-        print("Running test suite \(testClass)\n")
+        print("Running test suite \(testClass)")
         let tests = testClass as! XCTestCase.Type
         let testSuite = tests.defaultTestSuite()
         testSuite.run()
         let run = testSuite.testRun as! XCTestSuiteRun
         
-        print("Ran \(run.executionCount) tests in \(run.testDuration)s with \(run.totalFailureCount) failures\n")
+        print("\n *** Ran \(run.executionCount) tests in \(run.testDuration)s with \(run.totalFailureCount) failures ***\n")
     }
     
 }
 // **************** END OF BOILERPLATE CODE FOR RUNNING TESTS TO OUTPUT IN COMMAND LINE **************** //
 
-// ENTER TEST CLASSES TO RUN HERE: 
-TestRunner().runTests(testClass: MyExampleTests.self)
+
+// ENTER TEST CLASSES TO RUN HERE:
+
+TestRunner().runTests(testClass: MyTests.self)
+TestRunner().runTests(testClass: MyOtherTest.self)
+
